c30dc1f595706b70f0a5cf96274124584abd1a3b
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-13 12:25:09 -0300

    Criação do projeto

---
+++ partidas/src/test/java/com/futebol/partidas/PartidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futebol.partidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
