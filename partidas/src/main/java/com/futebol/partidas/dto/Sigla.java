57144f6bd50ffa8d63eea67848de99acbdbd7bfb
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-14 20:41:04 -0300

    Criado Enum Sigla

---
+++ partidas/src/main/java/com/futebol/partidas/dto/Sigla.java
@@ -0,0 +1,30 @@
+package com.futebol.partidas.dto;
+
+public enum Sigla {
+    AC,
+    AL,
+    AP,
+    AM,
+    BA,
+    CE,
+    DF,
+    ES,
+    GO,
+    MA,
+    MT,
+    MS,
+    MG,
+    PA,
+    PB,
+    PR,
+    PE,
+    PI,
+    RJ,
+    RN,
+    RS,
+    RO,
+    RR,
+    SC,
+    SP,
+    SE
+}
