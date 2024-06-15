2fbaf39d797333800ecbccda6700bc279ac55547
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-15 19:54:06 -0300

    Criado método inativarPorId

--- partidas/src/main/java/com/futebol/partidas/service/ClubeService.java
+++ partidas/src/main/java/com/futebol/partidas/service/ClubeService.java
@@ -1,6 +1,9 @@
 package com.futebol.partidas.service;
 
 import com.futebol.partidas.entity.ClubeEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.Optional;
 
@@ -10,4 +13,7 @@ public interface ClubeService {
 
     Optional<ClubeEntity> buscarPorId(Long id);
 
+    void inativarPorId(Long id);
+
+    Page<ClubeEntity> listar(Pageable paginacao);
 }
