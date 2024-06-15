1ff0bd44942c761630d7099a37abb33313a619e3
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-15 14:40:14 -0300

    Criação do método PUT

--- partidas/src/main/java/com/futebol/partidas/controller/ClubeController.java
+++ partidas/src/main/java/com/futebol/partidas/controller/ClubeController.java
@@ -4,13 +4,13 @@ import com.futebol.partidas.dto.request.ClubeRequestDTO;
 import com.futebol.partidas.dto.response.ClubeResponseDTO;
 import com.futebol.partidas.entity.ClubeEntity;
 import com.futebol.partidas.service.ClubeServiceImpl;
+import com.futebol.partidas.exception.NotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clubes")
@@ -23,17 +23,38 @@ public class ClubeController {
     }
 
     @PostMapping
-    public ResponseEntity<ClubeResponseDTO> cadastrar(@RequestBody ClubeRequestDTO clubeRequestDTO) {
+    public ResponseEntity<ClubeResponseDTO> salvar(@RequestBody ClubeRequestDTO clubeRequestDTO) {
 
         ClubeEntity clubeEntity = new ClubeEntity();
 
         BeanUtils.copyProperties(clubeRequestDTO, clubeEntity);
 
-        clubeEntity = clubeServiceImpl.cadastrar(clubeEntity);
+        clubeEntity = clubeServiceImpl.salvar(clubeEntity);
 
         ClubeResponseDTO clubeResponseDTO = new ClubeResponseDTO(clubeEntity.getId(), clubeEntity.getNome(),
                 clubeEntity.getSigla(), clubeEntity.getData(), clubeEntity.getAtivo());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(clubeResponseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClubeResponseDTO> editar(@PathVariable Long id, @RequestBody ClubeRequestDTO clubeRequestDTO) {
+
+        Optional<ClubeEntity> clubeEntityOptional = clubeServiceImpl.buscarPorId(id);
+
+        if (clubeEntityOptional.isEmpty()) {
+
+            throw new NotFoundException("Id não encontrado");
+        }
+
+        ClubeEntity clubeEntityEditado = new ClubeEntity(id, clubeRequestDTO.nome(),
+                clubeRequestDTO.sigla(), clubeRequestDTO.data(), clubeRequestDTO.ativo());
+
+        clubeEntityEditado = clubeServiceImpl.salvar(clubeEntityEditado);
+
+        ClubeResponseDTO clubeResponseDTO = new ClubeResponseDTO(clubeEntityEditado.getId(), clubeEntityEditado.getNome(),
+                clubeEntityEditado.getSigla(), clubeEntityEditado.getData(), clubeEntityEditado.getAtivo());
+
+        return ResponseEntity.ok(clubeResponseDTO);
     }
 }
