b101ebd92f33e48b4f247b1f35cdb2fcd42db10a
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-15 19:53:23 -0300

    Criado construtor

--- partidas/src/main/java/com/futebol/partidas/dto/response/ClubeResponseDTO.java
+++ partidas/src/main/java/com/futebol/partidas/dto/response/ClubeResponseDTO.java
@@ -1,8 +1,14 @@
 package com.futebol.partidas.dto.response;
 
 import com.futebol.partidas.dto.Sigla;
+import com.futebol.partidas.entity.ClubeEntity;
 
 import java.time.LocalDate;
 
 public record ClubeResponseDTO(Long id, String nome, Sigla sigla, LocalDate data, Boolean ativo) {
+
+    public ClubeResponseDTO(ClubeEntity clubeEntity) {
+        this(clubeEntity.getId(), clubeEntity.getNome(), clubeEntity.getSigla(),
+                clubeEntity.getData(),clubeEntity.getAtivo());
+    }
 }
