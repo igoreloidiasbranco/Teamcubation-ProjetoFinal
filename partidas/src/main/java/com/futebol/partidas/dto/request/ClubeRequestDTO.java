f9bdd4a0b001121e8f156096cce05cac35c5a0ce
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-16 11:54:20 -0300

    Adicionado anotações de validação

--- partidas/src/main/java/com/futebol/partidas/dto/request/ClubeRequestDTO.java
+++ partidas/src/main/java/com/futebol/partidas/dto/request/ClubeRequestDTO.java
@@ -1,8 +1,27 @@
 package com.futebol.partidas.dto.request;
 
 import com.futebol.partidas.dto.Sigla;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+
 
 import java.time.LocalDate;
 
-public record ClubeRequestDTO(String nome, Sigla sigla, LocalDate data, Boolean ativo) {
+public record ClubeRequestDTO(
+
+        @NotBlank
+        @Pattern(regexp = "^.{2,}$")
+        String nome,
+
+        @NotNull
+        Sigla sigla,
+
+        @NotNull
+        @Past
+        LocalDate data,
+
+        @NotNull
+        Boolean ativo) {
 }
