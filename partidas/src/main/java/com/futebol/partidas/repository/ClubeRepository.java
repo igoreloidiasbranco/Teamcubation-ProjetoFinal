505f32e18595bd8210764329385a9f8bb233bdf2
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-17 20:13:08 -0300

    Adicionado método existsByNomeAndSigla

--- partidas/src/main/java/com/futebol/partidas/repository/ClubeRepository.java
+++ partidas/src/main/java/com/futebol/partidas/repository/ClubeRepository.java
@@ -1,8 +1,10 @@
 package com.futebol.partidas.repository;
 
+import com.futebol.partidas.dto.Sigla;
 import com.futebol.partidas.entity.ClubeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubeRepository extends JpaRepository<ClubeEntity, Long> {
 
+    boolean existsByNomeAndSigla(String nome, Sigla sigla);
 }
