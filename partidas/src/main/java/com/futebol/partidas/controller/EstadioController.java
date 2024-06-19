0dccfc83aa0caee1e1e1c16486de9b2634946695
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-19 17:08:37 -0300

    Criação método PUT editar

--- partidas/src/main/java/com/futebol/partidas/controller/EstadioController.java
+++ partidas/src/main/java/com/futebol/partidas/controller/EstadioController.java
@@ -3,6 +3,7 @@ package com.futebol.partidas.controller;
 import com.futebol.partidas.dto.request.EstadioRequestDTO;
 import com.futebol.partidas.dto.response.EstadioResponseDTO;
 import com.futebol.partidas.entity.EstadioEntity;
+import com.futebol.partidas.exception.NotFoundException;
 import com.futebol.partidas.service.EstadioServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
@@ -11,7 +12,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/estadios")
@@ -43,5 +47,27 @@ public class EstadioController {
     public ResponseEntity<Page<EstadioResponseDTO>> listar(@PageableDefault(size = 5, sort = {"nome"})Pageable paginacao){
         Page page = estadioServiceImpl.listar(paginacao);
         return ResponseEntity.ok(page);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<EstadioResponseDTO> editar(@PathVariable Long id , @RequestBody EstadioRequestDTO estadioRequestDTO) {
+
+        Optional<EstadioEntity> estadioEntityOptional = estadioServiceImpl.buscarPorId(id);
+
+        if (estadioEntityOptional.isEmpty()) {
+
+            throw new NotFoundException("Id não encontrado");
+        }
+
+        EstadioEntity estadioEntityEditado = new EstadioEntity(id,
+                estadioRequestDTO.nome(), estadioRequestDTO.sigla());
+
+        estadioEntityEditado = estadioServiceImpl.salvar(estadioEntityEditado);
+
+        EstadioResponseDTO estadioResponseDTO = new EstadioResponseDTO(estadioEntityEditado.getId(),
+                estadioEntityEditado.getNome(), estadioEntityEditado.getSigla());
+
+        return ResponseEntity.ok(estadioResponseDTO);
     }
 }
